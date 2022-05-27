cda6773e92e4bfbd462787b1f0f7a7e4e483d601
Rasujon3 <rasujon3@gmail.com>
2022-05-28 01:46:11 +0600

    typescript 1st program

---
+++ first-program/index.js
@@ -0,0 +1,11 @@
+var userName = "anisul islam";
+userName = "123";
+// let age = 25;
+// console.log(userName);
+// console.log(age);
+function addNumber(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumber(20, 30);
+addNumber(20, 50);
+// indes.ts -> tsc index.ts -> index.js -> node index.js
---
+++ first-program/index.ts
@@ -0,0 +1,13 @@
+let userName: string = "anisul islam";
+userName = "123";
+// let age = 25;
+// console.log(userName);
+// console.log(age);
+
+function addNumber(num1: number, num2: number) {
+  console.log(num1 + num2);
+}
+addNumber(20, 30);
+addNumber(20, 50);
+
+// indes.ts -> tsc index.ts -> index.js -> node index.js
