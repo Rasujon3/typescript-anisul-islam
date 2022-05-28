8f7c6dc7c0d71b4bb502401fc1ed849a95028ff9
Rasujon3 <rasujon3@gmail.com>
2022-05-28 12:41:36 +0600

    6. Array data type

---
+++ user-defined-type/array-type.js
@@ -0,0 +1,8 @@
+// let userNames: string[];
+var userNames;
+userNames = ["anis", "rabu", "pinky"];
+console.log(userNames[0]);
+var multipleTypes;
+multipleTypes = [1, "name"];
+userNames.sort();
+console.log(userNames);
---
+++ user-defined-type/array-type.ts
@@ -0,0 +1,12 @@
+// let userNames: string[];
+let userNames: Array<string>;
+
+userNames = ["anis", "rabu", "pinky"];
+
+console.log(userNames[0]);
+
+let multipleTypes: (string | number)[];
+multipleTypes = [1, "name"];
+
+userNames.sort();
+console.log(userNames);
