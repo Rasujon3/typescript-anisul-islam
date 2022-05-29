55070d2715dad634e97cba63e6eec7fbc9664567
Rasujon3 <rasujon3@gmail.com>
2022-05-29 11:43:22 +0600

    15. Encapsulation & access modifiers

--- class-typescript/abstraction-demo.ts
+++ class-typescript/abstraction-demo.ts
@@ -1,28 +1,28 @@
-abstract class User {
-  userName: string;
-  age: number;
+// abstract class User {
+//   userName: string;
+//   age: number;
 
-  constructor(userName: string, age: number) {
-    this.userName = userName;
-    this.age = age;
-  }
+//   constructor(userName: string, age: number) {
+//     this.userName = userName;
+//     this.age = age;
+//   }
 
-  abstract display(): void;
-}
+//   abstract display(): void;
+// }
 
-class Student extends User {
-  studentId: number;
+// class Student extends User {
+//   studentId: number;
 
-  constructor(userName: string, age: number, studentId: number) {
-    super(userName, age);
-    this.studentId = studentId;
-  }
-  display(): void {
-    console.log(
-      `username: ${this.userName}, age: ${this.age}, id: ${this.studentId}`
-    );
-  }
-}
+//   constructor(userName: string, age: number, studentId: number) {
+//     super(userName, age);
+//     this.studentId = studentId;
+//   }
+//   display(): void {
+//     console.log(
+//       `username: ${this.userName}, age: ${this.age}, id: ${this.studentId}`
+//     );
+//   }
+// }
 
-let student1 = new Student("keya", 31, 1302020015);
-student1.display();
+// let student1 = new Student("keya", 31, 1302020015);
+// student1.display();
